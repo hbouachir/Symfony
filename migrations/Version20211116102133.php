8a398608491139f1358993d3b151c697f64231b1
hbouachir <hamza.bouachir@esprit.tn>
2021-11-16 13:44:48 +0100

    v0.1

---
+++ migrations/Version20211116102133.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211116102133 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE experience (id INT AUTO_INCREMENT NOT NULL, client_id_id INT DEFAULT NULL, note INT NOT NULL, avis VARCHAR(255) NOT NULL, INDEX IDX_590C103DC2902E0 (client_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE experience ADD CONSTRAINT FK_590C103DC2902E0 FOREIGN KEY (client_id_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE experience');
+    }
+}
